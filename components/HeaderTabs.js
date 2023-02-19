17eaa0b2756700b073f0a9bd2afdcf08874b8cdf
kamesh666 <mskkamesh4@gmail.com>
2023-02-19 19:52:08 +0530

    components & screens page

---
+++ components/HeaderTabs.js
@@ -0,0 +1,12 @@
+import {Text, View } from 'react-native'
+import React from 'react'
+
+const HeaderTabs = () => {
+  return (
+    <View>
+      <Text>HeaderTabs</Text>
+    </View>
+  )
+}
+
+export default HeaderTabs
---
+++ screens/Home.js
@@ -0,0 +1,13 @@
+import { SafeAreaView } from "react-native";
+import React from "react";
+import HeaderTabs from "../components/HeaderTabs";
+
+const Home = () => {
+  return (
+    <SafeAreaView>
+      <HeaderTabs />
+    </SafeAreaView>
+  );
+};
+
+export default Home;
